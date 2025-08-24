b79f3ec3b59659cb23b68798e1acc7f602e3278b
rhocci <4tres1@gmail.com>
2025-08-24 20:08:33 +0900

    feat: week2 - App 컴포넌트에 ThemeProvider 추가(+다크모드 state)

--- apps/week2/src/App.jsx
+++ apps/week2/src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './styles/theme.js';
 import Form from './components/Form/Form.jsx';
 
 function App() {
+  const [isDark, setisDark] = useState(false);
+
   return (
-    <>
+    <ThemeProvider theme={isDark ? darkTheme : lightTheme}>
       <Form />
-    </>
+    </ThemeProvider>
   );
 }
 
