39e329a75527c649e7dd69310fe34ff09a3d26cb
rhocci <4tres1@gmail.com>
2025-08-24 20:00:32 +0900

    chore: week2 - 스타일가이드 theme 객체 추가

---
+++ apps/week2/src/styles/theme.js
@@ -0,0 +1,95 @@
+/** 라이트모드 테마 */
+export const lightTheme = {
+  colors: {
+    primary: {
+      normal: '#3578FF',
+      hover: '#5B8DFF',
+      active: '#1855F5',
+      disabled: 'rgba(53,120,255,.5)',
+    },
+    success: {
+      normal: '#02B748',
+      hover: '#0ADD5C',
+      active: '#05903C',
+      disabled: 'rgba(2,183,72,.5)',
+    },
+    info: {
+      normal: '#1157FF',
+      hover: '#327CFF',
+      active: '#023DF3',
+      disabled: 'rgba(17,87,255,.5)',
+    },
+    warning: {
+      normal: '#F65900',
+      hover: '#FF7610',
+      active: '#CE4000',
+      disabled: 'rgba(246,89,0,.5)',
+    },
+    danger: {
+      normal: '#F71F1B',
+      hover: '#FF3834',
+      active: '#D00905',
+      disabled: 'rgba(247,31,27,.5)',
+    },
+    text: {
+      title: '#0B0B0C',
+      body: '#26262C',
+      subtle: '#4E4E5F',
+      caption: '#9B9DAE',
+    },
+    bg: { light: '#FAFAFA', medium: '#F4F4F5', dark: '#E3E3EB' },
+    stroke: { light: '#F4F4F5', medium: '#D2D2DA', dark: '#6D6D7E' },
+  },
+};
+
+/** 다크모드 테마 */
+export const darkTheme = {
+  colors: {
+    primary: {
+      normal: '#3578FF',
+      hover: '#5B8DFF',
+      active: '#1855F5',
+      disabled: 'rgba(53,120,255,.5)',
+    },
+    success: {
+      normal: '#02B748',
+      hover: '#0ADD5C',
+      active: '#05903C',
+      disabled: 'rgba(2,183,72,.5)',
+    },
+    info: {
+      normal: '#1157FF',
+      hover: '#327CFF',
+      active: '#023DF3',
+      disabled: 'rgba(17,87,255,.5)',
+    },
+    warning: {
+      normal: '#F65900',
+      hover: '#FF7610',
+      active: '#CE4000',
+      disabled: 'rgba(246,89,0,.5)',
+    },
+    danger: {
+      normal: '#F71F1B',
+      hover: '#FF3834',
+      active: '#D00905',
+      disabled: 'rgba(247,31,27,.5)',
+    },
+    text: {
+      title: '#FAFAFA',
+      body: '#E3E3EB',
+      subtle: '#9B9DAE',
+      caption: '#4E4E5F',
+    },
+    bg: {
+      light: '#0B0B0C',
+      medium: '#17171C',
+      dark: '#26262C',
+    },
+    stroke: {
+      light: '#17171C',
+      medium: '#3C3C40',
+      dark: '#6D6D7E',
+    },
+  },
+};
