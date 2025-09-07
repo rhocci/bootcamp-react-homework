8a390506f0bbd9be79084382b586ccc2e2eef4ce
rhocci <4tres1@gmail.com>
2025-09-07 17:29:19 +0900

    refactor: week4 - Sidebar+NavItem 컴포넌트 통합

--- apps/week4/src/components/Sidebar.tsx
+++ apps/week4/src/components/Sidebar.tsx
@@ -3,43 +3,44 @@ import { Profile } from "@/libs/supabase";
 import { tw } from "@/utils";
 
 type Props = {
-  to: string;
   user: Partial<Profile> | null;
-  authRequired?: boolean;
-  children: React.ReactNode;
 };
 
 export default function Sidebar({ user }: Props) {
+  const navClasses =
+    "rounded-l-full py-1.5 pr-7 pl-8 text-right font-[500] text-white hover:bg-white hover:font-[700] hover:text-indigo-800";
+  const activeNavClasses = "bg-white font-[700] text-indigo-800";
+
   return (
     <aside className="hidden bg-indigo-600 pb-10 pl-20 md:flex md:items-end">
       <nav className="flex flex-col gap-y-2">
-        <NavItem to="/" user={user}>
+        <NavLink
+          to="/"
+          aria-disabled={!user && undefined}
+          className={({ isActive }) =>
+            tw(navClasses, isActive && activeNavClasses)
+          }
+        >
           Home
-        </NavItem>
-        <NavItem to="/profile" user={user} authRequired>
+        </NavLink>
+        <NavLink
+          to="/profile"
+          aria-disabled={!user && undefined}
+          className={({ isActive }) =>
+            tw(navClasses, isActive && activeNavClasses)
+          }
+        >
           Profile
-        </NavItem>
-        <NavItem to={user ? "/signup" : "/signin"} user={user}>
-          {user ? "Logout" : "Login"}
-        </NavItem>
+        </NavLink>
+        <NavLink
+          to="/signup"
+          className={({ isActive }) =>
+            tw(navClasses, isActive && activeNavClasses)
+          }
+        >
+          Logout
+        </NavLink>
       </nav>
     </aside>
-  );
-}
-
-function NavItem({ to = "/", user, authRequired, children }: Props) {
-  return (
-    <NavLink
-      to={to}
-      aria-disabled={authRequired ? !user : undefined}
-      className={({ isActive }) =>
-        tw(
-          "rounded-l-full py-1.5 pr-7 pl-8 text-right font-[500] text-white hover:bg-white hover:font-[700] hover:text-indigo-800",
-          isActive && "bg-white font-[700] text-indigo-800",
-        )
-      }
-    >
-      {children}
-    </NavLink>
   );
 }
