d04bdc015e7801ffb3d38afd7db25d0ce48f6312
rhocci <4tres1@gmail.com>
2025-09-07 17:54:59 +0900

    feat: week4 - Input 패스워드 토글 아이콘, 기능 추가

--- apps/week4/src/components/Input.tsx
+++ apps/week4/src/components/Input.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { useFormContext, RegisterOptions } from "react-hook-form";
 import { tw } from "@/utils";
+import { Eye, EyeOff } from "lucide-react";
 
 type Props = {
   id?: string;
@@ -21,6 +23,12 @@ export default function Input({
     formState: { errors },
   } = useFormContext();
 
+  const [showPassword, setShowPassword] = useState(false);
+
+  function toggleShowPassword() {
+    setShowPassword((prev) => !prev);
+  }
+
   return (
     <div>
       <div className="m-2 flex items-center justify-between">
@@ -40,19 +48,30 @@ export default function Input({
       </div>
       <div
         className={tw(
-          "rounded-md bg-indigo-50 focus-within:outline-1 focus-within:outline-offset-1 focus-within:outline-indigo-600",
+          "flex gap-x-2 rounded-md bg-indigo-50 px-4 py-3 focus-within:outline-1 focus-within:outline-offset-1 focus-within:outline-indigo-600",
           errors[name] &&
             "outline outline-offset-1 outline-red-500 focus-within:outline-red-500",
         )}
       >
         <input
           {...register(name, rules)}
-          type={type}
+          type={showPassword ? "text" : type}
           id={id ?? name}
           name={name}
           {...props}
-          className="sm-text-sm/6 w-full grow px-4 py-3 text-base font-light text-gray-800 placeholder:text-gray-500 focus:outline-none"
+          className="sm-text-sm/6 w-full grow text-base font-light text-gray-800 placeholder:text-gray-500 focus:outline-none"
         />
+        {type === "password" && (
+          <button
+            type="button"
+            onClick={toggleShowPassword}
+            aria-label={showPassword ? "비밀번호 숨기기" : "비밀번호 표시"}
+            title={showPassword ? "비밀번호 숨기기" : "비밀번호 표시"}
+            className="text-gray-500"
+          >
+            {showPassword ? <EyeOff size={16} /> : <Eye size={16} />}
+          </button>
+        )}
       </div>
     </div>
   );
