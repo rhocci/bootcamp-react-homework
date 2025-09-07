d23593ae7234957787a788297202187658d8058f
rhocci <4tres1@gmail.com>
2025-09-07 17:33:35 +0900

    feat: week4 - 로그인 여부에 따라 로그인/로그아웃 조건부 렌더링

--- apps/week4/src/components/Sidebar.tsx
+++ apps/week4/src/components/Sidebar.tsx
@@ -4,12 +4,15 @@ import { tw } from "@/utils";
 
 type Props = {
   user: Partial<Profile> | null;
+  onLogout?: () => void;
 };
 
-export default function Sidebar({ user }: Props) {
+export default function Sidebar({ user, onLogout }: Props) {
   const navClasses =
     "rounded-l-full py-1.5 pr-7 pl-8 text-right font-[500] text-white hover:bg-white hover:font-[700] hover:text-indigo-800";
   const activeNavClasses = "bg-white font-[700] text-indigo-800";
+
+  function handleLogout() {}
 
   return (
     <aside className="hidden bg-indigo-600 pb-10 pl-20 md:flex md:items-end">
@@ -32,14 +35,20 @@ export default function Sidebar({ user }: Props) {
         >
           Profile
         </NavLink>
-        <NavLink
-          to="/signup"
-          className={({ isActive }) =>
-            tw(navClasses, isActive && activeNavClasses)
-          }
-        >
-          Logout
-        </NavLink>
+        {user ? (
+          <button onClick={onLogout} className={tw(navClasses)}>
+            Logout
+          </button>
+        ) : (
+          <NavLink
+            to="/signin"
+            className={({ isActive }) =>
+              tw(navClasses, isActive && activeNavClasses)
+            }
+          >
+            Login
+          </NavLink>
+        )}
       </nav>
     </aside>
   );
